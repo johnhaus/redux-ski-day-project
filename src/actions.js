d63f3797e39f3316de9b2835b70795bafa34f25a
johnhaus <john.haus@outlook.com>
2019-07-27 22:25:17 -0300

    action creators

--- src/actions.js
+++ src/actions.js
@@ -24,3 +24,26 @@ export const setGoal = (goal) =>
     type: C.SET_GOAL,
     payload: goal
   })
+
+export const addError = (message) =>
+  ({
+    type: C.ADD_ERROR,
+    payload: message
+  })
+
+export const clearError = (index) =>
+  ({
+    type: C.CLEAR_ERROR,
+    payload: index
+  })
+
+export const changeSuggestions = (suggestion) =>
+  ({
+    type: C.CHANGE_SUGGESTIONS,
+    payload: suggestion
+  })
+
+export const clearSuggestions = () =>
+  ({
+    type: C.CLEAR_SUGGESTIONS,
+  })
