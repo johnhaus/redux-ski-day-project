b331bf597e17d65ce3e6d8d1a111d9f7757cdd71
johnhaus <john.haus@outlook.com>
2019-07-27 22:47:49 -0300

    async

--- src/index.js
+++ src/index.js
@@ -1,72 +1,8 @@
-import expect from 'expect'
 import storeFactory from './store'
-
-import {
-  addDay,
-  removeDay,
-  setGoal,
-  addError,
-  clearError,
-  changeSuggestions,
-  clearSuggestions
-} from './actions'
+import { randomGoals } from './actions'
 
 const store = storeFactory()
 
 store.dispatch(
-  addDay("Heavenly", "2019-02-01")
+  randomGoals()
 )
-
-store.dispatch(
-  removeDay("2019-02-01")
-)
-
-store.dispatch(
-  setGoal(55)
-)
-
-// add error
-store.dispatch(
-  addError("something went wrong")
-)
-
-expect(store.getState().errors)
-  .toEqual(["something went wrong"])
-
-console.log(`
-  addError() Action Creator Works!!!
-`)
-
-// clear error
-store.dispatch(
-  clearError(0)
-)
-
-expect(store.getState().errors)
-  .toEqual([])
-
-console.log(`
-  clearError() Action Creator Works!!!
-`)
-
-// change suggestions
-store.dispatch(
-  changeSuggestions(['One', 'Two', 'Three'])
-)
-
-expect(store.getState().resortNames.suggestions)
-  .toEqual(['One', 'Two', 'Three'])
-
-console.log(`
-  changeSuggestions() Action Creator Works!!!
-`)
-
-// clear suggestions
-store.dispatch(clearSuggestions())
-
-expect(store.getState().resortNames.suggestions)
-  .toEqual([])
-
-console.log(`
-  clearSuggestions() Action Creator Works!!!
-`)
